cac12c842e77dd5828ff9a3bda3203a6e9eca5d2
tesinitsyn <sitiev174@gmail.com>
2023-11-12 14:10:40 +0300

    Initial commit

---
+++ src/main/java/com/tesinitsyn/gymserver/GymServerApplication.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.gymserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymServerApplication.class, args);
+    }
+
+}
